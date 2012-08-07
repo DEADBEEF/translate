44a564302a28c0e66ec9a0e8f86863ad82b621b8
Tim Trewartha <timminata@gmail.com>
2012-08-07 12:37:00 +0200

    Added admin panel and a basic database model

--- translator/main/models.py
+++ translator/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class translation(models.Model):
+    user = models.CharField(max_length=50)
+    filename = models.CharField(max_length=50)
+    comment = models.TextField()
+    translation = models.TextField()
