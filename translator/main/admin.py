5ba5c20ebf574580faf3f81d58855eb453178fbe
Tim Trewartha <timminata@gmail.com>
2012-08-12 13:07:24 +0200

    Added the extra models required for parsing

--- translator/main/admin.py
+++ translator/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from main.models import *
 
-admin.site.register(translation)
+admin.site.register(Translation)
+admin.site.register(Notebook)
+admin.site.register(Story)
+admin.site.register(Page)
--- translator/main/models.py
+++ translator/main/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class translation(models.Model):
+class Translation(models.Model):
     user = models.CharField(max_length=50)
-    filename = models.CharField(max_length=50)
+    filename = models.TextField()
     comment = models.TextField()
     translation = models.TextField()
+
+class Notebook(models.Model):
+    title = models.CharField(max_length=50,primary_key=True)
+    short_title = models.CharField(max_length=10,db_index=True)
+
+class Story(models.Model):
+    notebook = models.ForeignKey('Notebook')
+    title = models.CharField(max_length=300)
+    created = models.CharField(max_length=200)
+    description = models.TextField()
+    comment = models.TextField()
+    contributor = models.TextField()
+    subject = models.TextField()
+    keyword = models.TextField()
+
+class Page(models.Model):
+    story = models.ForeignKey('Story')
+    filename = models.TextField()
+    uuid = models.CharField(max_length=40,db_index=True)
