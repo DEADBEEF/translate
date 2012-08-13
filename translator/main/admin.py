b11dbcfd840afd3e419aa380e04eb05c4ffed2bf
Tim Trewartha <timminata@gmail.com>
2012-08-13 10:44:20 +0200

    merge conflict

--- translator/main/admin.py
+++ translator/main/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from main.models import *
 
-admin.site.register(Translation)
-admin.site.register(Notebook)
-admin.site.register(Story)
-admin.site.register(Page)
+class NotebookAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('id','notebook','title','pages')
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('filename',)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('user','story')
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('project','page')
+
+admin.site.register(Notebook,NotebookAdmin)
+admin.site.register(Story,StoryAdmin)
+admin.site.register(Page,PageAdmin)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Translation,TranslationAdmin)
