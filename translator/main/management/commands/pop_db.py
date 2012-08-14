eede54454ce6658aafa13c2fb5a090b82e7ed196
zuch <saschawatermeyer@gmail.com>
2012-08-14 10:52:23 +0200

    edited pop_db

--- translator/main/management/commands/pop_db.py
+++ translator/main/management/commands/pop_db.py
@@ -7,14 +7,10 @@ from main.models import Page, Notebook, Story
 from xml.dom.minidom import parse, parseString # DOM
 
 #globals
-<<<<<<< HEAD
 top = "" #root directory of stories archive
 story_path = "/translate/archive/lloydbleek/stories/"
 s_count = 0 #story count
 n_count = 0 #notebook count
-=======
-top = "/home/ttrewartha/www/archive/lloydbleek/stories/" #root directory of stories archive
->>>>>>> origin/master
 
 #node class
 class node:
@@ -41,8 +37,8 @@ class Command(BaseCommand):
       popdb()
       end = time.clock()
       print 'Code time %.2f seconds' % (end - start)
-	  
-	  
+
+
 #returns array of node objects for a given path
 def parse_path(path):
   
@@ -65,14 +61,14 @@ def popdb():
     if(i == 0):#populate notebook models
       nb_meta = node.files
       for filename in nb_meta:
-	notebook_meta(filename)
+notebook_meta(filename)
     
     if (len(node.path) > len(top) ):#populate story models
       s_meta = node.files
       for filename in s_meta:
-	path = node.path
-	story_meta(filename, path)
-	
+path = node.path
+story_meta(filename, path)
+
     i+=1
 
 #parses notebook metadata files
@@ -87,7 +83,7 @@ def notebook_meta(filename):
   notebook = Notebook(title=nb, short_title=short)
   notebook.save()
   
-  hasPart = [resource.getElementsByTagName("dcterms:hasPart")] 
+  hasPart = [resource.getElementsByTagName("dcterms:hasPart")]
   n_count+=1
   
 #parses story metadata files
@@ -120,25 +116,24 @@ def story_meta(filename, path):
   try:
     temp = dom.getElementsByTagName("bl:keywords")[0]
     for word in temp.getElementsByTagName("bl:keyword"):
-	k = word.getElementsByTagName("bl:kw")[0].firstChild.nodeValue
-	subkw = word.getElementsByTagName("bl:subkeywords")[0]
-	v = ", ".join(sub.firstChild.nodeValue
-		for sub in subkw.getElementsByTagName("bl:subkw"))
-	raw_keywords.append((k, v))
+k = word.getElementsByTagName("bl:kw")[0].firstChild.nodeValue
+subkw = word.getElementsByTagName("bl:subkeywords")[0]
+v = ", ".join(sub.firstChild.nodeValue
+for sub in subkw.getElementsByTagName("bl:subkw"))
+raw_keywords.append((k, v))
   except AttributeError:
     pass
   keyword = json.dumps(raw_keywords)
   notebook = Notebook.objects.get(short_title=notebook_name)
   
-  story = Story(notebook=notebook, title=story_title, created=date, description=description, 
-		comment=comments, contributor=contrib, subject=subject, keyword=keyword, pages=pages )
+  story = Story(notebook=notebook, title=story_title, created=date, description=description,
+comment=comments, contributor=contrib, subject=subject, keyword=keyword, pages=pages )
   story.save()
-  i=1
+  
   for page in required:
     page_path = page.firstChild.nodeValue
     page_uuid = uuid.uuid4()
-    page_entry = Page(story = story, filename = page_path, uuid = page_uuid,number=i)
-    i+=1
+    page_entry = Page(story = story, filename = page_path, uuid = page_uuid)
     page_entry.save()
   
   print "# pages: ", pages
